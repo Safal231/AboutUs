2b51ca5975087b3da463658a07feb5bcd9883b01
Safal231 <91495635+Safal231@users.noreply.github.com>
2022-04-07 00:19:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        }
+        else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+        $(".dark-mode-btn i").toggleClass("active");
+    })
+
+    $(".dark-mode-btn").click(function () {
+        $(".dark-mode-btn i").toggleClass("active2");
+    })
+});
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? "dark" : "light";
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+    );
+}
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darkTheme);
+    document.getElementById("home").classList.toggle(bgTheme);
+    localStorage.setItem("selected-theme", getCurrentTheme());
+});
